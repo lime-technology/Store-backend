4d3bd8284b00af969351e5541f04f83f29b1f6f2
Lime Technology <vipinkumar01.ws@gmail.com>
2026-03-20 22:29:41 +0530

    Simplify app.listen by removing host parameter

--- server.js
+++ server.js
@@ -52,6 +52,6 @@ app.post('/analyze', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-});
+})
